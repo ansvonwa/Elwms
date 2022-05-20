ce50d5b4ca99c9aaae5798caff5786addc876463
Anselm von Wangenheim <anselm.w@gmx.de>
2022-05-20 13:07:57 +0200

    add Elwms.scala

---
+++ src/main/scala/scala/collection/immutable/Elwms.scala
@@ -0,0 +1,13 @@
+package scala.collection.immutable
+
+class Elwms[+A] extends IndexedSeq[A] {
+
+
+
+  override def apply(i: Int): A = ???
+  override def length: Int = ???
+}
+
+object Elwms {
+
+}

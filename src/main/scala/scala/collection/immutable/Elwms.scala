03f6040207f8941c91433da529b76538238618d1
Anselm von Wangenheim <anselm.w@gmx.de>
2022-05-22 15:16:29 +0200

    introduce mpre tests and measure goal1{Fast,Slow}Factor

--- src/main/scala/scala/collection/immutable/Elwms.scala
+++ src/main/scala/scala/collection/immutable/Elwms.scala
@@ -3,8 +3,11 @@ package scala.collection.immutable
 class Elwms[+A](underlying: Vector[A]) extends IndexedSeq[A] {
 
 
-  override def apply(i: Int): A = underlying.apply(i)
+  override def apply(i: Int): A = underlying(i)
   override def length: Int = underlying.length // ???
+  override def iterator: Iterator[A] = underlying.iterator
+  override def headOption: Option[A] = underlying.headOption
+  override def map[B](f: A => B): Elwms[B] = new Elwms(underlying.map(f))
 }
 
 object Elwms {
--- src/test/scala/scala/collection/immutable/ElwmsGoalTest.scala
+++ src/test/scala/scala/collection/immutable/ElwmsGoalTest.scala
@@ -1,10 +1,19 @@
 package scala.collection.immutable
 
+import org.scalatest.Assertion
 import org.scalatest.funspec.AnyFunSpec
 
+import scala.collection.immutable.ElwmsGoalTest.{_goal1FastFactorRes, _goal1SlowFactorRes}
+
+/**
+ * Simple tests of Elwms' speed.
+ * Purpose is not to make scientifically clean benchmarks, but to get a hint
+ * when introducing stupid regressions.
+ */
 //noinspection ScalaUnusedExpression
 class ElwmsGoalTest extends AnyFunSpec {
 
+  val eps = 1e-6 // 1 µs
   val goal1FastFactor = 4.0
   val goal1SlowFactor = 2.0
   @`inline` def goal1Factor(isFast: Boolean): Double = if (isFast) goal1FastFactor else goal1SlowFactor
@@ -34,18 +43,23 @@ class ElwmsGoalTest extends AnyFunSpec {
                        isFast: Boolean = false,
                        emptyOther: Seq[Int] = List(),
                        init: Seq[Int] => Seq[Int] = identity
-                     ): Unit = {
+                     ): Assertion = {
     val elwms = init(Elwms[Int]())
     val other = init(emptyOther)
     val repetitions = n(isFast)
     val c = goal1Factor(isFast)
     val timeOther = time(op, repetitions, other) // Must be computed before, otherwise a cold JVM will cause the test to fail
     val timeElwms = time(op, repetitions, elwms)
-    assert(timeElwms <= c * timeOther)
+
+    val ass = assert(timeElwms <= c * timeOther + eps)
+    val ratio = timeElwms / timeOther
+    if (isFast && _goal1FastFactorRes < ratio) _goal1FastFactorRes = ratio
+    if (!isFast && _goal1SlowFactorRes < ratio) _goal1SlowFactorRes = ratio
+    ass
   }
 
   def testOp(op: Seq[Int] => Unit, toStr: String, isListFast: Boolean, isVectorFast: Boolean): Unit = {
-    def name(seqName: String) = toStr.replace("_", seqName)
+    def name(seqName: String) = toStr.replaceFirst("_", seqName)
     describe(name("Elwms")) {
       for ((emptyOther, isFast) <- Seq(List() -> isListFast, Vector() -> isVectorFast))
         describe(s"compared to ${name(emptyOther.toString.dropRight(2))}") {
@@ -62,5 +76,19 @@ class ElwmsGoalTest extends AnyFunSpec {
   testOp(_.size, "_.size", isListFast = true, isVectorFast = true)
   testOp(_.headOption, "_.headOption", isListFast = true, isVectorFast = true)
   testOp(_.lastOption, "_.lastOption", isListFast = false, isVectorFast = true)
+  testOp(_.iterator.hasNext, "_.iterator.hasNext", isListFast = true, isVectorFast = true)
 
+  testOp(_.sum, "_.sum", isListFast = false, isVectorFast = false)
+  testOp(_.map(x => 2*x), "_.map(x => 2*x)", isListFast = false, isVectorFast = false)
+
+  // hacky output
+  it("print output") {
+    println(s"fast operations took at most ${_goal1FastFactorRes.toFloat} times (<$goal1FastFactor) as long")
+    println(s"slow operations took at most ${_goal1SlowFactorRes.toFloat} times (<$goal1SlowFactor) as long")
+  }
+}
+
+object ElwmsGoalTest {
+  private var _goal1FastFactorRes = 0.0
+  private var _goal1SlowFactorRes = 0.0
 }
